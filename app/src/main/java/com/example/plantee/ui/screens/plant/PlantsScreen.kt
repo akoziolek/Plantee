1c3b851f1de820cd30186d3247c188a7bcc6fc59
Koziolek <koziolola43@gmail.com>
2026-05-20 14:13:44 +0200

    fix: add missing imePadding

--- app/src/main/java/com/example/plantee/ui/screens/plant/PlantsScreen.kt
+++ app/src/main/java/com/example/plantee/ui/screens/plant/PlantsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
@@ -66,7 +67,7 @@ fun PlantsScreen(
             )
         },
         floatingActionButton = {
-            PrimaryFloatingButton(text = stringResource(R.string.plants_btn_add), onClick = { viewModel.onAddClick() })
+            PrimaryFloatingButton(text = stringResource(R.string.plants_btn_add), onClick = { viewModel.onAddClick() }, modifier = Modifier.imePadding())
         },
     ) { innerPadding ->
         if (state.isLoading) {
--- app/src/main/java/com/example/plantee/ui/screens/routine/RoutinesScreen.kt
+++ app/src/main/java/com/example/plantee/ui/screens/routine/RoutinesScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
@@ -81,7 +82,8 @@ fun RoutinesScreen(
         floatingActionButton = {
             PrimaryFloatingButton(
                 text = stringResource(R.string.routines_btn_add),
-                onClick = { viewModel.onAddClick() })
+                onClick = { viewModel.onAddClick() },
+                modifier = Modifier.imePadding())
         }
     ) { innerPadding ->
         if (state.isLoading) {
